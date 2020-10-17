271a820665d26de82c634e2060e144fc7a367c9c
Enrique Pedroza <alu0100886351@ull.edu.es>
2020-10-17 14:06:20 +0100

    codigo comentado

--- operacion.h
+++ operacion.h
@@ -1,22 +1,26 @@
 #ifndef OPERACION_H
 #define OPERACION_H
+
+/** Alumno: Enrique Manuel Pedroza Castillo
+    Alu: alu0100886351
+    Clase: Operacion
+    Esta clase describe a un objeto contiene a la operacion cuyo resultado es un numero capicuo
+*/
 class Operacion {
  public:
-  Operacion();
-  Operacion(int, int, int);
-  void setMultiplos(int, int);
-  void setCapicua(int);
-  int getMultiplo1();
-  int getMultiplo2();
-  int getCapicua();
-  friend bool operator<(const Operacion&, const Operacion&);
-  friend bool operator==(const Operacion&, const Operacion&);
+  Operacion();                        // constructor por defecto
+  Operacion(int, int, int);           // constructor parametrizado con los atributos de los multiplos y el numero resultante capicuo
+  void setMultiplos(int, int);        // setter de los multiplos 
+  void setCapicua(int);               // setter del numero resultante capicuo
+  int getMultiplo1();                 // getter del primer multiplo
+  int getMultiplo2();                 // getter del segundo multiplo
+  int getCapicua();                   // getter del numero resultado capicuo
+  friend bool operator<(const Operacion&, const Operacion&);    // operador <
+  friend bool operator==(const Operacion&, const Operacion&);   // operador ==
   
  private:
   int m1;
   int m2;
   int capicua;
-
-
 };
 #endif
--- palindrome_prod.h
+++ palindrome_prod.h
@@ -4,24 +4,33 @@
 #include <fstream>
 #include <set>
 #include "operacion.h"
+
+/** Alumno: Enrique Manuel Pedroza Castillo
+    Alu: alu0100886351
+    Clase: PalindromeProd
+    Esta clase describe a un objeto que recibe un numero entero 'n' y un fichero de salida
+    su finalidad es escribir en el fichero aquellos numeros palindromos que se obtengan
+    del resultado de multiplicar dos numeros naturales con longitud de 'n' digitos
+*/
+
 #ifndef PALINDROME_PROD_H
 #define PALINDROME_PROD_H
 using namespace std;
 class PalindromeProd {
  public:
-  PalindromeProd();
-  PalindromeProd(int, char*);
-  int CrearNumero(int);
-  bool IsPalondrome(int);
-  void CrearProductos();
-  void EscribirFichero();
+  PalindromeProd();   // Constructor por defecto
+  PalindromeProd(int, char*); // Constructor por parametros que recibe el numero n y el fichero de salida
+  int CrearNumero(int);     // metodo que recibe un numero 'n' y devuelva el primer numero que contenga longitud 'n'
+  bool IsPalondrome(int);   // metodo que recibe un numero y nos devuelve un booleano que nos indicara si es palindromo (true) o no (false)
+  void CrearProductos();    // metodo que realiza los productos y encuentra los productos que resultan en numeros palindromos
+  void EscribirFichero();   // metodo que escribira las multiplicaciones que resultan en palindromos dentro del fichero especificado
   
  private:
-  int N;
-  int PrimerNumero;
-  int UltimoNumero;
-  set<Operacion> ListaOperaciones;
-  fstream Output;
+  int N;                      // N es el numero natural que define la longitud de los multiplicandos
+  int PrimerNumero;           // es el primer numero dentro del rango 
+  int UltimoNumero;           // ultimo numero del rango 
+  set<Operacion> ListaOperaciones;    // set que contiene las operaciones cuyo resultado es un numero capicuo
+  fstream Output;             // stream hacia el fichero de salida
 
 
 };
